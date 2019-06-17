44418a715525448e772eaecbb517c1543e2b461f
Sam Shareski <sam_shareski@otpp.com>
2019-06-17 17:56:27 -0400

    Add simple live clock

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule TenThousandDollarHomepage.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"}
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:calendar, "~> 0.17.6"}
     ]
   end
 
